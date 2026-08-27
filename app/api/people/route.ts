f9a973840ad21f157fe926a1b005a9995f3e39c0
winesty <winesty@gmail.com>
2026-08-27 17:17:35 +0900

    Update route.ts

--- app/api/people/route.ts
+++ app/api/people/route.ts
@@ -3,6 +3,7 @@ import { getStorage } from "@/lib/storage";
 import { Person } from "@/lib/types";
 import { nanoid } from "nanoid";
 
+export const dynamic = "force-dynamic";
 export async function GET() {
   const storage = await getStorage();
   const people = await storage.listPeople();
